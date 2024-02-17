4474c9239efbc68817ddebe5ff7dd876678524fa
Chavez <nimbus.arc@outlook.com>
2024-02-17 15:37:38 +0800

    Add:  ...

--- app/Http/Controllers/HobbiesController.php
+++ app/Http/Controllers/HobbiesController.php
@@ -8,7 +8,7 @@ class HobbiesController extends Controller
 {
     public function index()
     {
-        $data = "I love playing games, reading about programming languages, and foreign languages. Most importantly, I thoroughly enjoy discovering new things that I don’t know or that might possibly happen.";
+        $data = "I love playing games, reading about programming languages, and foreign languages. Most importantly, I thoroughly enjoy discovering new things that I don’t know or that might possibly happen...";
         return view('hobbies',compact('data'));
     }
 }
